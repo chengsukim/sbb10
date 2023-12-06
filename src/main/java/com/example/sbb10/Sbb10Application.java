0ab7fa969206240465459765f9716c99850b0450
깃chengsukim <kcs981123@naver.com>
2023-12-06 09:21:47 +0900

    sbb10

---
+++ src/main/java/com/example/sbb10/Sbb10Application.java
@@ -0,0 +1,13 @@
+package com.example.sbb10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb10Application.class, args);
+	}
+
+}
